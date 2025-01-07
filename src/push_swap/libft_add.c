4ea15d33b648f8d3df4b960c40c21325d227e8e6
Frauke Von Der Haar <fvon-der@student.42heilbronn.de>
2025-01-07 17:31:20 +0100

    WP : Utils
    TODO : add these functions tp libft

---
+++ src/push_swap/libft_add.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft_add.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fvon-der <fvon-der@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/06 20:23:56 by fvon-der          #+#    #+#             */
+/*   Updated: 2024/08/10 08:31:46 by fvon-der         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/push_swap.h"
+
+int	ft_issign(int c)
+{
+	if (c == '+' || c == '-')
+		return (1);
+	return (0);
+}
+
+//check if character is space character
+//TODO get rid off, use libft
+int	ft__isspace(int c)
+{
+	if (c == ' ')
+		return (1);
+	return (0);
+}
+
+void	ft_freestr(char **lst)
+{
+	char	**temp;
+
+	if (!lst)
+		return ;
+	temp = lst;
+	while (*temp)
+	{
+		free(*temp);
+		temp++;
+	}
+	temp = lst;
+	while (*temp)
+	{
+		temp++;
+	}
+	if (temp)
+	{
+		*temp = NULL;
+	}
+}
